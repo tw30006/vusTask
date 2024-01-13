2f6ce7f27a384fe63e0a7c884a05fe5cd017b5f8
林郁軒 <sponge@linyuxuandeMacBook-Air.local>
2024-01-13 19:46:34 +0800

    update login page

---
+++ week2/login.js
@@ -0,0 +1,29 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+createApp({
+    data(){
+        return({
+            user:{
+                username: '',
+                password: ''
+            }
+            
+        })
+    },
+    methods:{
+        login(){
+            const apiUrl='https://vue3-course-api.hexschool.io/v2';
+            const path='sponge'
+
+            axios.post(`${apiUrl}/admin/signin`,this.user)
+            .then((res)=>{
+                const { token,expired }=res.data;
+                document.cookie = `hexToken=${ token }; expires=${ new Date(expired) };`;
+                window.location="productApi.html"
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+        }
+    }
+}).mount('#app')
